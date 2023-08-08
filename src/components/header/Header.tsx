69f9ac6a675d652c0d6f0100aa26d2e297b7ed29
elina <elina.nep@gmail.com>
2023-08-08 14:10:27 +0200

    feat: add SDK

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
+import { getProject } from '../../sdk/GetInfo';
 
 export const Header = () => {
+  getProject().then(console.log).catch(console.error);
   return (
     <header className="header">
       this is header
---
+++ src/sdk/BuildClient.ts
@@ -0,0 +1,38 @@
+import fetch from 'node-fetch';
+import {
+  ClientBuilder,
+
+  // Import middlewares
+  type AuthMiddlewareOptions, // Required for auth
+  type HttpMiddlewareOptions, // Required for sending HTTP requests
+} from '@commercetools/sdk-client-v2';
+
+export const projectKey = process.env.REACT_APP_PROJECT_KEY || '';
+const scopes = process.env.REACT_APP_SCOPES?.split(' ') || [
+  'view_published_products:beefront',
+];
+
+// Configure authMiddlewareOptions
+const authMiddlewareOptions: AuthMiddlewareOptions = {
+  host: 'https://auth.europe-west1.gcp.commercetools.com',
+  projectKey: projectKey,
+  credentials: {
+    clientId: process.env.REACT_APP_CLIENT_ID || '',
+    clientSecret: process.env.REACT_APP_CLIENT_SECRET || '',
+  },
+  scopes,
+  fetch,
+};
+
+// Configure httpMiddlewareOptions
+const httpMiddlewareOptions: HttpMiddlewareOptions = {
+  host: 'https://api.europe-west1.gcp.commercetools.com',
+  fetch,
+};
+
+// Export the ClientBuilder
+export const ctpClient = new ClientBuilder()
+  .withClientCredentialsFlow(authMiddlewareOptions)
+  .withHttpMiddleware(httpMiddlewareOptions)
+  .withLoggerMiddleware() // Include middleware for logging
+  .build();
