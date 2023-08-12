4e18e69c18f094649ce9b77127d45c1e086d4f6e
halinad <gaxin@list.ru>
2023-08-12 17:48:02 +0300

    feat: add head of header

---
+++ src/components/header/Head.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Head.css';
+
+interface HeadProps {
+  cartTotal: number;
+  cartItemsCount: number;
+}
+
+const Head: React.FC<HeadProps> = ({ cartTotal, cartItemsCount }) => {
+  return (
+    <div className="head">
+      <div className="headnav">
+        <div className="logo">
+          <div className="logo-picture">
+            <img src={process.env.PUBLIC_URL + 'assets/вата.png'} alt="logo" />
+          </div>
+          <div className="store-title">
+            <Link to="/" className="store-name">
+              Enchant Fest
+            </Link>
+            <br />
+            <span className="slogan">Unleash the Magic of Festivals</span>
+          </div>
+        </div>
+        <div className="cart">
+          <Link to="cart">
+            CART / ${cartTotal.toFixed(2)} gds: {cartItemsCount}
+          </Link>
+        </div>
+
+        <div className="links">
+          <Link to="login" className="login">
+            Sign in
+          </Link>
+          <Link to="register" className="register">
+            Sign up
+          </Link>{' '}
+          <br />
+          <Link to="logout" className="logout">
+            Log out
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Head;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,9 @@
-import { Link } from 'react-router-dom';
+import Head from './Head';
 
 export const Header = () => {
   return (
     <header className="header">
-      this is header
-      <Link to="login">login page</Link>
-      <Link to="register">register page</Link>
-      <Link to="/">main page</Link>
+      <Head cartTotal={0} cartItemsCount={0} />
     </header>
   );
 };
