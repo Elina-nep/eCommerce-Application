285a1829d9ba0d1f198ccccd41ad065e851c8925
halinad <gaxin@list.ru>
2023-08-15 15:09:21 +0300

    feat: add error page

--- src/pages/404/NotFound.tsx
+++ src/pages/404/NotFound.tsx
@@ -1,10 +1,29 @@
 import { Link } from 'react-router-dom';
+import { Footer } from '../../components/footer/Footer';
+import { Header } from '../../components/header/Header';
+import './NotFound.css';
 
 export const NotFound = () => {
   return (
-    <div>
-      Oops 404
-      <Link to="/">go to Main page</Link>
-    </div>
+    <>
+      <Header />
+      <div className="not-found-page">
+        <div>
+          <img
+            src="./assets/swinging.gif"
+            alt="404 Image"
+            className="not-found-page-img"
+          ></img>
+        </div>
+        <div className="not-found-page-text">
+          <h3>
+            Something‘s wrong here... We can‘t find page you‘re looking for.
+            Check it out or head back to home
+          </h3>
+          <Link to="/">Home</Link>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 };
