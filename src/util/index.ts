b51b8eeb4d21caa93ae0f05da2e9ead6a61e84da
halinad <gaxin@list.ru>
2023-08-19 23:48:22 +0300

    refactor: change structure with util and index

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,32 +1,4 @@
-import { LinkItem, ColumnItem } from '../types/footer';
-
-export const links: LinkItem[] = [
-  { title: 'Home', to: '/' },
-  { title: 'Catalog', to: '#9' },
-  { title: 'About us', to: '#10' },
-];
-
-export const catalogLinks: LinkItem[] = [
-  { title: 'Birthday', to: '#3' },
-  { title: 'Decorations', to: '#4' },
-  { title: 'Balloons', to: '#5' },
-  { title: 'Lightnings', to: '#6' },
-  { title: 'Party Supplies', to: '#7' },
-  { title: 'Gifts', to: '#8' },
-];
-
-export const columns: ColumnItem[] = [
-  { id: 'information', title: 'Information', links },
-  { id: 'catalog', title: 'Catalog', links: catalogLinks },
-  {
-    id: 'my-account',
-    title: 'My account',
-    links: [
-      { title: 'My room', to: '#1' },
-      { title: 'My cart', to: '#2' },
-    ],
-  },
-];
+export { links, catalogLinks, columns } from './footerConstants';
 export { tokenCache, getExistingToken } from './tokenProcess';
 export {
   nameValidation,
