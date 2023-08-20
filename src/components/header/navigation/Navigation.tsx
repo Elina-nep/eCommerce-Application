d38e7a6338263139c7f3c7b73381df16258039c5
Iryna Zhebryk <irinazhebrik@gmail.com>
2023-08-20 21:16:58 +0200

    feat: responsive navigation

--- src/components/header/navigation/Navigation.tsx
+++ src/components/header/navigation/Navigation.tsx
@@ -1,25 +1,42 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Navigation.css';
 import { ReactComponent as InputIcon } from '../../../assets/inputIcon.svg';
+import './Navigation.scss';
 
 export function Navigation() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="nav">
       <div className="navigation-container">
-        <Link to="#">Catalog</Link>
-        <Link to="#">My room</Link>
-        <Link to="#">About us</Link>
-        <div className="navigation-text-field-container">
-          <input
-            className="navigation-text-field__input"
-            type="search"
-            name="find"
-            id="find"
-            placeholder="Search"
-          />
-          <span className="navigation-text-field__aicon">
-            <InputIcon />
-          </span>
+        <button
+          className={`menu-toggle ${menuOpen ? 'active' : ''}`}
+          onClick={toggleMenu}
+        >
+          <div className="bar" />
+          <div className="bar" />
+          <div className="bar" />
+        </button>
+        <div className={`menu-links ${menuOpen ? 'active' : ''}`}>
+          <Link to="#">Catalog</Link>
+          <Link to="#">My room</Link>
+          <Link to="#">About us</Link>
+          <div className="navigation-text-field-container">
+            <input
+              className="navigation-text-field__input"
+              type="search"
+              name="find"
+              id="find"
+              placeholder="Search"
+            />
+            <span className="navigation-text-field__aicon">
+              <InputIcon />
+            </span>
+          </div>
         </div>
       </div>
     </nav>
