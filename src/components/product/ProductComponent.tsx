971635d10f79f885fda6354bdea7ef3705e37298
Iryna Zhebryk <irinazhebrik@gmail.com>
2023-09-01 17:13:09 +0200

    feat: restyle product page with discount

--- src/components/product/ProductComponent.tsx
+++ src/components/product/ProductComponent.tsx
@@ -8,7 +8,7 @@ import {
   CURRENCY,
   formatAttributes,
   getProductAttribute,
-  getProductCategories,
+  // getProductCategories,
   getProductImages,
   getProductPrice,
   getProductPriceDiscounted,
@@ -19,7 +19,7 @@ import { ProductModal } from './assets/ProductModal';
 
 export const ProductComponent: React.FC<IProductComponentProps> = ({
   product,
-  categories,
+  // categories,
 }) => {
   const title = product.current.name[LANGUAGE.EN] || '';
   const description = product.current.description?.en || '';
@@ -28,11 +28,11 @@ export const ProductComponent: React.FC<IProductComponentProps> = ({
   const occasions = getProductAttribute(product, 'occasions', LANGUAGE.EN);
   const price = getProductPrice(product, CURRENCY.EUR);
   const discountedPrice = getProductPriceDiscounted(product, CURRENCY.EUR);
-  const productCategories = getProductCategories(
-    product,
-    categories,
-    LANGUAGE.EN,
-  );
+  // const productCategories = getProductCategories(
+  //   product,
+  //   categories,
+  //   LANGUAGE.EN,
+  // );
   const productImages = getProductImages(product);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
@@ -76,49 +76,56 @@ export const ProductComponent: React.FC<IProductComponentProps> = ({
           modalImageIndex={currentImageIndex}
         />
       )}
-      <div className="product__slider">
-        <Button onClick={showPreviousImage} className="button-pagination">
-          {'<'}
-        </Button>
-        <div className="product__image">
-          {productImages?.length && (
-            <img
-              onClick={handleOpenModal}
-              src={productImages[currentImageIndex]}
-              alt={title}
-            />
-          )}
-        </div>
+      <div className="product__content">
+        <div className="product__slider">
+          <Button onClick={showPreviousImage} className="button-pagination">
+            {'<'}
+          </Button>
+          <div className="product__image">
+            {productImages?.length && (
+              <img
+                onClick={handleOpenModal}
+                src={productImages[currentImageIndex]}
+                alt={title}
+              />
+            )}
+          </div>
 
-        <Button onClick={showNextImage} className="button-pagination">
-          {'>'}
-        </Button>
-      </div>
-      <h1 className="product__title">{title}</h1>
-      {discountedPrice ? (
-        <div className="product__with_discount">
-          <p className="product__offer">Special Offer</p>
-          {price && <p className="product__price">{price}</p>}
-          {<p className="product__dicounted_price">{discountedPrice}</p>}
+          <Button onClick={showNextImage} className="button-pagination">
+            {'>'}
+          </Button>
         </div>
-      ) : (
-        <div className="product__no_discount">
-          {price && <p className="product__price">{price}</p>}
-        </div>
-      )}
-      <div className="product__categories">
-        {productCategories.map((category, index) => (
-          <p key={index}>{category}</p>
-        ))}
-      </div>
-      <div className="product__details">
-        {description && <p className="product__description">{description}</p>}
-        <div className="product__attributes">
-          {material.length > 1 && <p>Material: {formatAttributes(material)}</p>}
-          {color.length > 1 && <p>Color: {formatAttributes(color)}</p>}
-          {occasions.length > 1 && (
-            <p>Occasions: {formatAttributes(occasions)}</p>
+        <div className="product__content_details">
+          <h1 className="product__title">{title}</h1>
+          {discountedPrice ? (
+            <div className="product__with_discount">
+              <p className="product__offer">Special Offer</p>
+
+              <div className="product__prices_box">
+                {price && <p className="product__price">{price}</p>}
+                {<p className="product__dicounted_price">{discountedPrice}</p>}
+              </div>
+            </div>
+          ) : (
+            <div className="product__no_discount">
+              {price && <p className="product__price">{price}</p>}
+            </div>
           )}
+          {/* <div className="product__categories">
+            {productCategories.map((category, index) => (
+              <p key={index}>{category}</p>
+            ))}
+          </div> */}
+          <div className="product__details">
+            {description && (
+              <p className="product__description">{description}</p>
+            )}
+            <div className="product__attributes">
+              {material && <p>Material: {formatAttributes(material)}</p>}
+              {color && <p>Color: {formatAttributes(color)}</p>}
+              {occasions && <p>Occasions: {formatAttributes(occasions)}</p>}
+            </div>
+          </div>
         </div>
       </div>
     </div>
