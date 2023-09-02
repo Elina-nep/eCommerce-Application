6597556daf9d3b02af39b7e1bbb4d3e2559e4c23
halinad <gaxin@list.ru>
2023-09-03 02:00:36 +0300

    feat: add product cards wraper styles

--- src/components/catalog/filtersComponent/priceFilter/PriceFilter.tsx
+++ src/components/catalog/filtersComponent/priceFilter/PriceFilter.tsx
@@ -36,6 +36,7 @@ export const PriceFilter = ({
       <div>
         <ThemeProvider theme={priceTheme}>
           <Slider
+            sx={{ marginTop: '1rem' }}
             getAriaLabel={() => 'Price range'}
             value={[priceFrom, priceTo]}
             max={200}
