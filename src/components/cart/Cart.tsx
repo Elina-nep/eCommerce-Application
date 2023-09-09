4aa66d190e89b50a5e9d1c2acf054b23353d203c
Iryna Zhebryk <irinazhebrik@gmail.com>
2023-09-09 16:33:11 +0200

    refactor: coupon component

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,79 +1,26 @@
 import './Cart.scss';
 
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import { AuthContext } from '../../context/AuthProvider';
 import {
   changeItemInCart,
   CURRENCY,
-  discountCart,
   getCartBeforeCoupon,
   getCartDiscount,
   getCartTotalPrice,
-  getDiscount,
 } from '../../util';
 import Button from '../buttons/Button';
+import { Coupon } from './coupon/Coupon';
 import { ItemInCart } from './item/ItemInCart';
 
 export const Cart = () => {
   const { cart, setCart } = useContext(AuthContext);
-  const [code, setCode] = useState({ code: '', id: '' });
-  const [errorMessage, setErrorMessage] = useState('');
 
   const totalBeforeDiscount = getCartBeforeCoupon(cart, CURRENCY.SYMBOL);
   const discount = getCartDiscount(cart, CURRENCY.SYMBOL);
   const finalTotal = getCartTotalPrice(cart, CURRENCY.SYMBOL);
-
-  useEffect(() => {
-    if (cart.discountCodes.length > 0) {
-      const code = cart.discountCodes.find((el) => el.state === 'MatchesCart');
-      if (code) {
-        getDiscount(code.discountCode.id)
-          .then((res) => {
-            console.log(res);
-            setCode({ code: res.code, id: res.id });
-          })
-          .catch((e) => {
-            console.log(e.message);
-          });
-      }
-    } else setCode({ code: '', id: '' });
-  }, [cart]); // это для того, чтобы отображать примененный к корзине и активный купон, можно перенести в отдельный компонент
-
-  const [couponInput, setCouponInput] = useState('');
-
-  const handleAddDiscount = (couponInput: string) => {
-    discountCart({
-      discount: couponInput,
-      cartVersion: cart.version,
-      cartId: cart.id,
-      action: 'addDiscountCode',
-    })
-      .then((res) => {
-        setCart(res);
-        setErrorMessage('');
-        setCouponInput('');
-      })
-      .catch((e) => {
-        console.log(e.message);
-        setErrorMessage(e.message || 'An error occurred');
-      });
-  };
-  const handleDeleteDiscount = (id: string) => {
-    discountCart({
-      discountCode: id,
-      cartVersion: cart.version,
-      cartId: cart.id,
-      action: 'removeDiscountCode',
-    })
-      .then((res) => {
-        setCart(res);
-      })
-      .catch((e) => {
-        console.log(e.message);
-      });
-  };
 
   const handleClearCart = () => {
     const itemsQuantities: number[] = [];
@@ -142,44 +89,7 @@ export const Cart = () => {
               </p>
             </div>
           </div>
-
-          <div className="coupon__new">
-            <input
-              placeholder="COUPON"
-              value={couponInput}
-              onChange={(e) => setCouponInput(e.target.value)}
-              className="coupon__new_input"
-            />
-            <button
-              onClick={() => handleAddDiscount(couponInput)}
-              className="coupon__new_btn"
-            >
-              Apply
-            </button>
-          </div>
-          {errorMessage && <p className="coupon__new_error">{errorMessage}</p>}
-
-          {cart.discountCodes.length ? (
-            <div className="coupon__current">
-              <div className="coupon__current_container">
-                <p className="coupon__current_title">Applied coupon: </p>
-                <div className="coupon__current_content">
-                  <span className="coupon__current_code">{code.code}</span>
-                  <button
-                    className="coupon__current_delete_btn"
-                    onClick={() => handleDeleteDiscount(code.id)}
-                  >
-                    x
-                  </button>
-                </div>
-              </div>
-              <p className="coupon__current_tip">
-                Please note, only one discount can be applied at a time.
-              </p>
-            </div>
-          ) : (
-            ''
-          )}
+          <Coupon />
         </div>
       </div>
     </div>
---
+++ src/components/cart/coupon/Coupon.tsx
@@ -0,0 +1,105 @@
+import './Coupon.scss';
+
+import { useContext, useEffect, useState } from 'react';
+import React from 'react';
+
+import { AuthContext } from '../../../context/AuthProvider';
+import { discountCart, getDiscount } from '../../../util';
+
+export const Coupon: React.FC = () => {
+  const { cart, setCart } = useContext(AuthContext);
+  const [code, setCode] = useState({ code: '', id: '' });
+  const [errorMessage, setErrorMessage] = useState('');
+  const [couponInput, setCouponInput] = useState('');
+
+  useEffect(() => {
+    if (cart.discountCodes.length > 0) {
+      const code = cart.discountCodes.find((el) => el.state === 'MatchesCart');
+      if (code) {
+        getDiscount(code.discountCode.id)
+          .then((res) => {
+            console.log(res);
+            setCode({ code: res.code, id: res.id });
+          })
+          .catch((e) => {
+            console.log(e.message);
+          });
+      }
+    } else setCode({ code: '', id: '' });
+  }, [cart]);
+
+  const handleAddDiscount = (couponInput: string) => {
+    discountCart({
+      discount: couponInput,
+      cartVersion: cart.version,
+      cartId: cart.id,
+      action: 'addDiscountCode',
+    })
+      .then((res) => {
+        setCart(res);
+        setErrorMessage('');
+        setCouponInput('');
+      })
+      .catch((e) => {
+        console.log(e.message);
+        setErrorMessage(e.message || 'An error occurred');
+      });
+  };
+  const handleDeleteDiscount = (id: string) => {
+    discountCart({
+      discountCode: id,
+      cartVersion: cart.version,
+      cartId: cart.id,
+      action: 'removeDiscountCode',
+    })
+      .then((res) => {
+        setCart(res);
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  };
+
+  return (
+    <div className="coupon">
+      <div className="coupon__new">
+        <input
+          placeholder="COUPON"
+          value={couponInput}
+          onChange={(e) => setCouponInput(e.target.value)}
+          className="coupon__new_input"
+        />
+        <button
+          onClick={() => handleAddDiscount(couponInput)}
+          className="coupon__new_btn"
+        >
+          Apply
+        </button>
+      </div>
+
+      {errorMessage && <p className="coupon__new_error">{errorMessage}</p>}
+
+      {cart.discountCodes.length ? (
+        <div className="coupon__current">
+          <div className="coupon__current_container">
+            <p className="coupon__current_title">Applied coupon: </p>
+            <div className="coupon__current_content">
+              <span className="coupon__current_code">{code.code}</span>
+              <button
+                className="coupon__current_delete_btn"
+                onClick={() => handleDeleteDiscount(code.id)}
+              >
+                x
+              </button>
+            </div>
+          </div>
+          <p className="coupon__current_tip">
+            Please note, only one discount can be applied at a time.
+          </p>
+        </div>
+      ) : (
+        ''
+      )}
+    </div>
+  );
+};
