40f05824ee442a5ab953f0e7c455d26dd11e755b
halinad <gaxin@list.ru>
2023-09-13 14:23:23 +0300

    feat: add test for toencache

--- tests/Components.test.tsx
+++ tests/Components.test.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 
 import LoadingSpinner from '../src/components/loading/LoadingSpinner';
+import { tokenCache } from '../src/util';
 
 describe('LoadingSpinner', () => {
   it('renders without errors', () => {
@@ -12,5 +13,45 @@ describe('LoadingSpinner', () => {
     const { container } = render(<LoadingSpinner />);
     const ball = container.querySelectorAll('[class^="ball-"]');
     expect(ball).toHaveLength(8);
+  });
+});
+
+const localStorageExample = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+};
+describe('tokenCache', () => {
+  beforeEach(() => {
+    localStorageExample.getItem.mockClear();
+    localStorageExample.setItem.mockClear();
+    Object.defineProperty(window, 'localStorage', {
+      value: localStorageExample,
+    });
+  });
+
+  it('should get token correctly from localStorage', () => {
+    const exampleToken = { token: 'exampleToken' };
+    localStorageExample.getItem.mockReturnValueOnce(
+      JSON.stringify(exampleToken),
+    );
+    const result = tokenCache.get();
+    expect(localStorageExample.getItem).toHaveBeenCalledWith('token');
+    expect(result).toEqual(exampleToken);
+  });
+
+  it('should set token correctly in localStorage', () => {
+    const exampleToken = { token: 'exampleToken', expirationTime: 0 };
+    tokenCache.set(exampleToken);
+    expect(localStorageExample.setItem).toHaveBeenCalledWith(
+      'token',
+      JSON.stringify(exampleToken),
+    );
+  });
+
+  it('should return null if token is not found in localStorage', () => {
+    localStorageExample.getItem.mockReturnValueOnce(null);
+    const result = tokenCache.get();
+    expect(localStorageExample.getItem).toHaveBeenCalledWith('token');
+    expect(result).toBeNull();
   });
 });
