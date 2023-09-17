1a29571fcfe75c943decaf56c98e893c04e6e30a
Iryna Zhebryk <irinazhebrik@gmail.com>
2023-09-17 18:46:00 +0200

    fix: profile buttons

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,11 +1,9 @@
 import { LoginForm } from '../../components/auth/LoginForm';
-import { Blob } from '../../components/blob/Blob';
 
 export const LoginPage: React.FC = () => {
   return (
     <div>
       <LoginForm />
-      <Blob componentHue={183} />
     </div>
   );
 };
