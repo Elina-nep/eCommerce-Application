7b34110bdbf6db5ac16bc23ce7874537e94b5a3b
elina <elina.nep@gmail.com>
2023-09-18 14:33:07 +0200

    feat: clear cart modal

---
+++ src/components/cart/modal/CartModal.tsx
@@ -0,0 +1,37 @@
+import './CartModal.scss';
+
+import React from 'react';
+
+import Button from '../../buttons/Button';
+
+export const CartModal = ({
+  closeModal,
+  handleClearCart,
+}: {
+  closeModal: () => void;
+  handleClearCart: () => void;
+}) => {
+  return (
+    <>
+      <div className="modal" onClick={closeModal}>
+        <div className="cart__modal-wrapper">
+          <p> Clear cart? </p>
+          <div>
+            <Button
+              onClick={() => {
+                closeModal();
+                handleClearCart();
+              }}
+              className="profile__edit_btn"
+            >
+              {'Yes'}
+            </Button>
+            <Button onClick={closeModal} className="profile__delete_btn">
+              {'No'}
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
